a1f9b03c7f6819c38fe594dc45718acbe3345783
Sam SEBILLE <sebille.sam@gmail.com>
2022-11-01 13:49:05 -0400

    test file

---
+++ app/src/androidTest/java/ca/qc/bdeb/c5gm/tp1moblie/StockageTest.java
@@ -0,0 +1,44 @@
+package ca.qc.bdeb.c5gm.tp1moblie;
+
+import static org.junit.Assert.*;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth.assertWithMessage;
+
+
+import org.junit.Test;
+
+public class StockageTest {
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onUpgrade() {
+    }
+
+    @Test
+    public void getEntreprises() {
+    }
+
+    @Test
+    public void getEntreprise() {
+    }
+
+    @Test
+    public void updateEntreprise() {
+    }
+
+    @Test
+    public void ajouterEntreprise() {
+    }
+
+    @Test
+    public void deleteEntreprise() {
+    }
+
+    @Test
+    public void dropTable() {
+    }
+}
