1582546ade211218051290a0f62c817332fc8ce4
Sam SEBILLE <sebille.sam@gmail.com>
2022-12-11 16:09:02 -0500

    stokage tests done

--- app/src/androidTest/java/ca/qc/bdeb/c5gm/tp1moblie/StockageTest.java
+++ app/src/androidTest/java/ca/qc/bdeb/c5gm/tp1moblie/StockageTest.java
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 import ca.qc.bdeb.c5gm.tp1moblie.Activities.Entreprise;
+import ca.qc.bdeb.c5gm.tp1moblie.Activities.Etudiant;
 import ca.qc.bdeb.c5gm.tp1moblie.BD.Stockage;
 
 
@@ -48,7 +49,7 @@ public class StockageTest {
 
     @Test
     public void getEntreprises() {
-        Entreprise entreprise1 = new Entreprise("", "", "", "", "", "", "", false);
+        Entreprise entreprise1 = new Entreprise(id,"", "", "", "", "", "", "", false);
         stockage.ajouterEntreprise(entreprise1);
         ArrayList<Entreprise> entreprises = stockage.getEntreprises();
         Truth.assertThat(entreprises).isNotNull();
@@ -56,7 +57,7 @@ public class StockageTest {
 
     @Test
     public void getEntreprise() {
-        Entreprise entreprise1 = new Entreprise("", "", "", "", "", "", "", false);
+        Entreprise entreprise1 = new Entreprise(id,"", "", "", "", "", "", "", false);
         stockage.ajouterEntreprise(entreprise1);
         Entreprise entreprises = stockage.getEntreprise(id);
         Truth.assertThat(entreprises).isNotNull();
@@ -64,7 +65,7 @@ public class StockageTest {
 
     @Test
     public void updateEntreprise() {
-        Entreprise entreprise = new Entreprise("", "", "", "", "", "", "", false);
+        Entreprise entreprise = new Entreprise(id,"", "", "", "", "", "", "", false);
         stockage.ajouterEntreprise(entreprise);
         UUID id = entreprise.getId();
         entreprise = new Entreprise(id, "test", "test", "test", "test", "test", "yul", "?", false);
@@ -85,13 +86,57 @@ public class StockageTest {
 
     @Test
     public void deleteEntreprise() {
-        Entreprise entreprise = new Entreprise("", "", "", "", "", "", "", false);
+        Entreprise entreprise = new Entreprise(id,"", "", "", "", "", "", "", false);
         stockage.ajouterEntreprise(entreprise);
         UUID id = entreprise.getId();
         boolean isSupr = false;
         stockage.deleteEntreprise(entreprise);
         try {
             stockage.getEntreprise(id);
+        } catch (CursorIndexOutOfBoundsException e) {// si l'entreprise n'est pas présente, getEntreprise() renvoi cette exception
+            isSupr = true;
+        }
+
+        Truth.assertThat(isSupr).isEqualTo(true);
+
+    }
+
+    @Test
+    public void ajouterEtudiant(){
+        Etudiant etudiant = new Etudiant(id,"","","",true,1000);
+        boolean ajout = true;
+        try {
+            stockage.ajouterEtudiant(etudiant);
+        } catch (Exception e) {
+            ajout = false;
+        }
+        Truth.assertThat(ajout).isTrue();
+    }
+    @Test
+    public void getEtudiant(){
+        Etudiant etudiant = new Etudiant(id,"","","",true,1000);
+        stockage.ajouterEtudiant(etudiant);
+        Etudiant etudiants = stockage.getEtudiant(id);
+        Truth.assertThat(etudiants).isNotNull();
+    }
+
+    @Test
+    public void getEtudiants(){
+        Etudiant etudiant = new Etudiant(id,"","","",true,1000);
+        stockage.ajouterEtudiant(etudiant);
+        ArrayList<Etudiant> etudiants = stockage.getEtudiants();
+        Truth.assertThat(etudiants).isNotNull();
+    }
+
+    @Test
+    public void deleteEtudiant(){
+        Etudiant etudiant = new Etudiant(id,"","","",true,1000);
+        stockage.ajouterEtudiant(etudiant);
+        UUID id = etudiant.getId();
+        boolean isSupr = false;
+        stockage.deleteEtudiant(etudiant);
+        try {
+            stockage.getEtudiant(id);
         } catch (CursorIndexOutOfBoundsException e) {// si l'entreprise n'est pas présente, getEntreprise() renvoi cette exception
             isSupr = true;
         }
